ab918078e9e8763216b0e94557a7fd8957145d6e
Kraziman <notkraziman@gmail.com>
2025-03-06 12:24:08 +0200

    example commit

--- .idea/HelloWorld.java
+++ .idea/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         if (true){
             System.out.println("Hello World");
-            System.out.println("Hello World");
+            System.out.println("World Hello");
             System.out.println("Hello World");
         }
     }
